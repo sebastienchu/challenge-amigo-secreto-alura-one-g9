25bf42713ab433f935d43aec01e6abd0793bfe8c
sebastian villarroel <sebastianalvarez12121@gmail.com>
2025-08-06 11:30:09 -0500

    añadí el array en el js
    comencé el reto  siguiendo e el paso a paso

---
+++ app.js
@@ -0,0 +1,4 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// crear un array aqui hice la lista vacia
+let amigosSecreto = []
+
