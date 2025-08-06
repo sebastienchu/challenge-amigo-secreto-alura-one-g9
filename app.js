72cc51bba27cd54fcd38fc23860c6a9205e1ea1c
sebastian villarroel <sebastianalvarez12121@gmail.com>
2025-08-06 13:26:53 -0500

    actualiza el app.js
    se añadio la lista y funciones

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 // crear un array aqui hice la lista vacia
-let amigosSecreto = []
+let amigosSecreto = [];
+
+// implementacion de la funcion para agregar amigos a la lista
+function agregarAmigo(){
+    // captura la informacion ingresada por el usuario
+    let amigosDelUsuario = document.getElementById('amigo').value;
+    // valida que el campo no este en blanco
+    if (amigosDelUsuario === "") {
+            alert('Por favor, inserte un nombre.');
+        } else {
+            //se cumple la condicion y se añade el nombre a la lista 
+            amigosSecreto.push(amigosDelUsuario);
+
+        limpiarCampo ();
+        }
+              
+        console.log(amigosDelUsuario);
+        console.log(amigosSecreto);
+        
+    }
+// funcion para limpiar el campo de entrada  y dejar listo para agregar mas nombres
+function limpiarCampo() {
+    document.getElementById('amigo').value = '';
+}
+
+    
+
 
