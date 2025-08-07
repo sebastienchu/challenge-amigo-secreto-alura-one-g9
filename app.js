355a33cb20f6cdaf7b90265bf0eb973a1f6f4114
sebastian villarroel <sebastianalvarez12121@gmail.com>
2025-08-07 12:21:19 -0500

    actualiza nuevas funciones del app.js
    en esta fase ya e incorporado la funcion de añadis nombres a la lista

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 // crear un array aqui hice la lista vacia
-let amigosSecreto = [];
+let amigos = [];
 
 // implementacion de la funcion para agregar amigos a la lista
 function agregarAmigo(){
@@ -11,20 +11,29 @@ function agregarAmigo(){
             alert('Por favor, inserte un nombre.');
         } else {
             //se cumple la condicion y se añade el nombre a la lista 
-            amigosSecreto.push(amigosDelUsuario);
-
-        limpiarCampo ();
+            amigos.push(amigosDelUsuario);
         }
-              
-        console.log(amigosDelUsuario);
-        console.log(amigosSecreto);
         
+        actualizarListaAmigos();
+        limpiarCampo ();
+        
+        return;        
     }
 // funcion para limpiar el campo de entrada  y dejar listo para agregar mas nombres
 function limpiarCampo() {
     document.getElementById('amigo').value = '';
+    return;
 }
+// funcion que genera la lista de amigos a sortear
+function actualizarListaAmigos(){
+    let listaDeAmigos = document.getElementById('listaAmigos');
+    listaDeAmigos.innerHTML = "";
+    for(let i = 0; i < amigos.length; i++) {
+        let listado = document.createElement('li');
+        listado.innerHTML = amigos[i];
+        listaDeAmigos.appendChild(listado);
+        document.getElementById('amigo').value = "";
+    }
 
     
-
-
+}
